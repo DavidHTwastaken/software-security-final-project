756c089affa108327a2b6e97dd5c6596d8d19111
Sid-26 <siddhantdas262@gmail.com>
2024-03-31 17:47:49 -0400

    fixed login form

--- app/src/static/login.js
+++ app/src/static/login.js
@@ -1,9 +1,10 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('login-form-submit').addEventListener('click', (event) => {
+    event.preventDefault()
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
-
+    console.log("handling login")
     if (username === '' || password === '') {
-        event.preventDefault();
+        
         alert('Please enter both username and password.')
     } else {
         const data = {
@@ -28,6 +29,4 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
             }
         )
     }
-
-
 });

c11e7293374c671ce549a5f308ee4328e0fbc44d
Sheida Ebrahimi <sheida1381@hotmail.com>
2024-03-31 19:10:52 -0400

    Update app.py

--- app/src/app.py
+++ app/src/app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from db import DB
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
+db = DB()
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = db.get_user(username, password)
+
+    if user:
+        return jsonify({'auth': True})
+    else:
+        return jsonify({'auth': False})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True)

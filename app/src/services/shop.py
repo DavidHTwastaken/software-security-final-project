7905eded0fc4d8dcf5d0ac2785d5b0033ba75f89
Sid-26 <siddhantdas262@gmail.com>
2024-04-13 15:38:53 -0400

    added handler code for buying and selling

--- app/src/services/shop.py
+++ app/src/services/shop.py
@@ -1,8 +1,40 @@
+from app.src.db import DB
+
+db = DB()
+
 class Shop:
     @staticmethod
-    def buy():
-        pass
+    def buy(username: str, id: int):
+        product = db.get_product(id)
+        balance = db.get_balance(username)
+
+        if None == product:
+            return "Product not found"
+
+        value = float(product['price'])
+
+        if value > balance:
+            return "Insufficient funds"
+
+        db.update_balance(username, value)
+        db.add_inventory(username,id)
+
+        if 5 == id:
+            return "Congratulations, you solved the challenge"
+        
+        return ""
 
     @staticmethod
-    def sell():
-        pass
+    def sell(username: str, id: int):
+        product = db.get_product_from_inventory(username, id)
+        balance = db.get_balance(username)
+
+        if None == product:
+            return "Product doesn't exist in inventory"
+        
+        value = float(product['price'])
+
+        db.update_balance(username, value)
+        db.remove_inventory(username,id)
+
+        return ""

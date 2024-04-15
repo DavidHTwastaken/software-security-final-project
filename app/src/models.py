10a85a0c712c56634552cc41a8a4f18b2e62867a
Sid-26 <siddhantdas262@gmail.com>
2024-04-15 02:47:14 -0400

    updated and actually added models

--- app/src/models.py
+++ app/src/models.py
@@ -3,6 +3,35 @@ from db import DB
 
 db = DB()
 
+class PeeweeDB(PostgresqlDatabase):
+    def __init__(self, db):
+        self.db = db
+        super().__init__('vulnerable')
+
+    def get_conn(self):
+        return self.db.connection
+
+orm_db = PeeweeDB(db)
+
 class BaseModel(Model):
     class Meta:
-        database = db
+        database = orm_db
+
+class Users(BaseModel):
+    user_id = AutoField(primary_key=True)
+    username = CharField(unique=True, max_length=50)
+    password = CharField(max_length=100)
+    balance = DecimalField(default=20.0)
+
+    class Meta:
+        table_name = 'users'
+
+class Products(BaseModel):
+    user_id = AutoField(primary_key=True)
+    class Meta:
+        table_name = 'products'
+
+class Inventory(BaseModel):
+
+    class Meta:
+        table_name = 'inventory'
